502e436fe1eb3228016b82ddff774ce2206ef2dd
YANGSANGHEE <tkdgmlek567@gmail.com>
2023-08-09 12:49:42 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, { useRef, useCallback } from 'react';
+import {
+  PinchZoom,
+  applyCssProperties,
+  getTransform3DValue,
+  getTransform2DValue,
+} from 'react-fast-pinch-zoom';
 import './App.css';
 
 function App() {
+  const ref = useRef(null);
+  const onTransform = useCallback(({ x, y, scale }) => {
+    // Use CSS Custom Properties (Variables)
+    applyCssProperties(ref.current, { x, y, scale });
+
+    // Or use literal transform value
+    // ref.current.style.setProperty('transform',getTransform3DValue({x,y,scale}));
+    // ref.current.style.setProperty('transform',getTransform2DValue({x,y,scale}));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PinchZoom onTransform={onTransform}>
+      <img
+        ref={ref}
+        alt='two black cats look at you'
+        src='https://t1.kakaocdn.net/thumb/R1920x0.fwebp.q100/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaocorp%2Fkakaocorp%2Fadmin%2Fnews%2Fbe7674ae018600001.jpeg'
+      />
+    </PinchZoom>
   );
 }
 
